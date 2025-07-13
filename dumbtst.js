0d1ea7927c5e33b4017a981b4a076946b3c32610
su <smalfhaid@gmail.com>
2025-07-13 04:55:41 +0300

    Firts js evergit status

--- dumbtst.js
+++ dumbtst.js
@@ -1,13 +1,58 @@
-class Animal {
-
+class Person{  
+    constructor(name = "Su", age = 22, energy = 70){
+        this.name = name
+        this.age = age
+        this.energy = energy
     }
 
-    class Cat extends Animal {
-      constructor() {
-        super();
-        this.noise = "meow";
-      }
+    sleep(){
+        (this.energy == 100)? this.energyStatus() : this.energy += 10
     }
 
-    var result = new Animal();
-    console.log(result.noise);
+    doSomethingFun(){
+        (this.energy == 0)? this.energyStatus() : this.energy -= 10
+    }
+
+    energyStatus(){
+        console.log(`${this.name} energy level is ${this.energy}`, 
+            (this.energy == 0)?", they need to sleep >:(":", they can still have some fun :3")
+        
+    }
+
+    bio(){
+        console.log(`Hi!! My name is ${this.name} and I am ${this.age} years old.`)
+    }
+}
+
+class Worker extends Person{
+    constructor(name = "Su", age = 22, energy = 70, hourlywage = 100, xp = 2000){
+        super(name, age, energy)
+        this.hourlywage = hourlywage
+        this.xp = xp       
+    }
+
+    goToWork(){
+        this.xp += 10
+    }
+}
+
+function intern(){
+    var obj = new Worker("Bob", 21, 120, 0, 10);
+    obj.goToWork()
+    return obj
+
+}
+
+function manager(){
+    var obj = new Worker("Alice", 30, 120, 100, 30);
+    obj.doSomethingFun()
+    return obj
+
+}
+
+var intern = intern()
+var manager = manager()
+
+
+
+
